0a30ca0978151fb99ae1b3a5978b239376f0a4ed
Canh.Nguyen <leagueyasuo186@gmail.com>
2022-02-11 10:49:35 +0700

    k

---
+++ src/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        student.change();
+        student hs1 = new student(1, "canh");
+        student hs2 = new student(2, "n.anh");
+        student hs3 = new student(3, "yuki");
+        hs1.display();
+        hs2.display();
+        hs3.display();
+    }
+}
